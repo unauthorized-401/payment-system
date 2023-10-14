8bfd8e6267233476c66a9fd68c8488de19f88bd7
Jiwon <moonlight_youlike@naver.com>
2023-10-14 16:57:40 +0900

    Completed POST /common/payment/retrieve API

--- src/main/java/com/jiwon/payment/controller/parameter/PaymentResponseParam.java
+++ src/main/java/com/jiwon/payment/controller/parameter/PaymentResponseParam.java
@@ -1,17 +1,15 @@
 package com.jiwon.payment.controller.parameter;
 
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
 /*
-    카드결제/결제취소 API
+    카드결제 API
 
     Response: 관리번호, string 데이터
 */
 @Getter @Setter
-public class ResultParam {
+public class PaymentResponseParam {
     private String id;
 
     private String data;
---
+++ src/main/java/com/jiwon/payment/controller/parameter/RetrieveResponseParam.java
@@ -0,0 +1,29 @@
+package com.jiwon.payment.controller.parameter;
+
+import com.jiwon.payment.entity.Payment;
+import lombok.Getter;
+import lombok.Setter;
+
+/*
+    결제정보조회 API
+
+    Response: 관리번호, 카드정보(카드번호, 유효기간, CVC), 결제/취소 구분, 금액정보(결제/취소 금액, 부가가치세)
+*/
+@Getter @Setter
+public class RetrieveResponseParam {
+    // TODO: NULL 값 허용, 값 범위 제한
+
+    private String id;
+
+    private String cardNumber;
+
+    private String expirationPeriod;
+
+    private String cvc;
+
+    private Payment.PAYMENT_TYPE type;
+
+    private long price;
+
+    private int vat;
+}
